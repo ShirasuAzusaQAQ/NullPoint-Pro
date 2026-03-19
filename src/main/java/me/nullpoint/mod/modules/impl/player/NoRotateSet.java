ce7119d2312093980ec1d09c6d6134cd9af88f8e
ShirasuAzusaQAQ <2057054937@qq.com>
2026-03-19 20:55:00 +0800

    修复了受到强制视角转换时，由NoRotateSet引发的视角故障的问题。

--- src/main/java/me/nullpoint/mod/modules/impl/player/NoRotateSet.java
+++ src/main/java/me/nullpoint/mod/modules/impl/player/NoRotateSet.java
@@ -6,6 +6,8 @@ import me.nullpoint.api.events.eventbus.EventPriority;
 import me.nullpoint.asm.accessors.IPlayerPositionLookS2CPacket;
 import me.nullpoint.mod.modules.Module;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import net.minecraft.network.packet.s2c.play.PositionFlag;
+import java.util.Set;
 
 public class NoRotateSet extends Module {
 	public static NoRotateSet INSTANCE;
@@ -18,8 +20,9 @@ public class NoRotateSet extends Module {
 	public void onPacketReceive(PacketEvent.Receive event){
 		if(nullCheck()) return;
 		if(event.getPacket() instanceof PlayerPositionLookS2CPacket packet){
-			float yaw = mc.player.getYaw();
-			float pitch = mc.player.getPitch();
+			Set<PositionFlag> flags = packet.getFlags();
+			float yaw = flags.contains(PositionFlag.Y_ROT) ? 0.0f : mc.player.getYaw();
+			float pitch = flags.contains(PositionFlag.X_ROT) ? 0.0f : mc.player.getPitch();
 			((IPlayerPositionLookS2CPacket) packet).setYaw(yaw);
 			((IPlayerPositionLookS2CPacket) packet).setPitch(pitch);
 		}
